e496289d58ba15ddc32e289c1b804b22ec2d6018
Rohak <rhs1080.99.work@gmail.com>
2025-04-07 18:25:56 +0530

    Migration and SuperUser

--- first_django_project/blog/views.py
+++ first_django_project/blog/views.py
@@ -1,29 +1,29 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
-posts = [
-    {
-        "author": "R",
-        "title": "One",
-        "content": "1 - lorem ipsum",
-        "date": "Today",
-    },
-    {
-        "author": "S",
-        "title": "Two",
-        "content": "2 - lorem ipsum",
-        "date": "Tomorrow",
-    },
-]
+# from django.http import HttpResponse
+# posts = [
+#     {
+#         "author": "R",
+#         "title": "One",
+#         "content": "1 - lorem ipsum",
+#         "date": "Today",
+#     },
+#     {
+#         "author": "S",
+#         "title": "Two",
+#         "content": "2 - lorem ipsum",
+#         "date": "Tomorrow",
+#     },
+# ]
 
 
 def blogHome(req):
     # Can send a dictionary of data as context
-    context = {
-        "posts": posts,
-    }
-    return render(req, "blog/home.html", context)
-    # return render(req, "blog/home.html")
+    # context = {
+    #     "posts": posts,
+    # }
+    # return render(req, "blog/home.html", context)
+    return render(req, "blog/home.html")
     # return HttpResponse("<h1>Blog Home</h1>")
 
 
