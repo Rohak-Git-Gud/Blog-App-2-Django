1631331672038e0066695748405047abd2593042
Rohak <rhs1080.99.work@gmail.com>
2025-04-24 16:58:56 +0530

    Blog CRUD

--- first_django_project/blog/urls.py
+++ first_django_project/blog/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from . import views
 
 urlpatterns = [
-    path('', views.blogHome, name="blogHome"),
-    path('about/', views.blogAbout, name="blogAbout"),
+    path("", views.blogHome, name="blogHome"),
+    path("about/", views.blogAbout, name="blogAbout"),
+
+    path("post/create/", PostCreateView.as_view(), name="blogCreate"),
+    # path("", PostListView.as_view(), name="blogHome"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="blogRead"),
+    path("post/update/<int:pk>/", PostUpdateView.as_view(), name="blogUpdate"),
+    path("post/delete/<int:pk>/", PostDeleteView.as_view(), name="blogDelete"),
 ]
--- first_django_project/blog/views.py
+++ first_django_project/blog/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # from django.http import HttpResponse
 # posts = [
@@ -36,3 +44,54 @@ def blogHome(req):
 
 def blogAbout(req):
     return render(req, "blog/about.html")
+
+
+# CRUD
+
+
+# C - Create
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ["title", "content"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+# R - Retrieve all
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/home.html"
+    # Looks for <app>/<model>_<view_type>.html be default.
+    context_object_name = "posts"
+    ordering = ["-date_posted"]
+
+
+# R - Retrieve one
+class PostDetailView(DetailView):
+    model = Post
+
+
+# U - Update
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ["title", "content"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+
+# D - Delete
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = "/"
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
