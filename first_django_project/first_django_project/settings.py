23b5e6b34df0fae24a997e324feb9acb31c0e353
Rohak <rhs1080.99.work@gmail.com>
2025-05-14 17:46:09 +0530

    Post Push Spring Cleaning

--- first_django_project/first_django_project/settings.py
+++ first_django_project/first_django_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-b8%!%_&9akys08rhrhk+-!ypaslzxq48)e)i1ki9atpy3w2hm_"
+SECRET_KEY = "ChangedSecretKeyPostPush___ITSDELIBERATELYINCORRECT"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
